c0993e1942acc7a8b072af08a4b03be94f84f1ff
Ivan Kenig <keniiig21@yandex.ru>
2022-11-14 20:26:06 +0300

    implement step four - add form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,4 +6,8 @@ module HexletCode
   autoload :Tag, 'hexlet_code/tag'
 
   class Error < StandardError; end
+
+  def self.form_for(_user, options = {})
+    "<form action=\"#{options[:url].nil? ? '#' : options[:url]}\" method=\"post\"></form>"
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,6 +3,8 @@
 require 'test_helper'
 
 class TestHexletCode < Minitest::Test
+  User = Struct.new(:name, :job, keyword_init: true)
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
@@ -10,5 +12,12 @@ class TestHexletCode < Minitest::Test
   def test_tag_build
     assert_equal('<img src="path/to/image">', HexletCode::Tag.build('img', src: 'path/to/image'))
     assert_equal('<label for="email">Email</label>', HexletCode::Tag.build('label', for: 'email') { 'Email' })
+  end
+
+  def test_form_for
+    user = User.new(name: 'rob')
+
+    assert_equal('<form action="#" method="post"></form>', HexletCode.form_for(user))
+    assert_equal('<form action="/users" method="post"></form>', HexletCode.form_for(user, url: '/users'))
   end
 end
